fc9f9c9977edad385c1ec05b700b700ee658b96a
euqen <e.shilin@paralect.com>
2016-02-27 18:35:27 +0300

    [+] Initial commit

---
+++ src/main/java/resources/actions/data/ProjectsAction.java
@@ -0,0 +1,55 @@
+package resources.actions.data;
+
+import java.sql.*;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Result;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <code>List Apache projects.</code>
+ */
+@Result(type = "json")
+public class ProjectsAction extends ActionSupport {
+
+    private static final long serialVersionUID = 9037336532369476225L;
+    private static final Logger log = LogManager.getLogger(ProjectsAction.class);
+
+    private List<String> projectNames;
+
+    public String execute() throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+
+        projectNames = new ArrayList<String>();
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        String DB_URL = "jdbc:mysql://localhost:3306/boostapp-core";
+
+        conn = DriverManager.getConnection(DB_URL, "root", "root");
+
+        stmt = conn.createStatement();
+
+        String sql = "SELECT * FROM projects";
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            String title = rs.getString("title");
+            projectNames.add(title);
+        }
+
+        rs.close();
+
+        return SUCCESS;
+
+    }
+
+    public List<String> getProjectNames() {
+        return projectNames;
+    }
+}
