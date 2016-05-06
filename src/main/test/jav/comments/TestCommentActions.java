5b55b7b524bee72825e56f56a964ff583081922d
Dzmitry <yanusikdima@mail.ru>
2016-05-06 21:12:28 +0300

    Added files via upload

---
+++ src/main/test/jav/comments/TestCommentActions.java
@@ -0,0 +1,13 @@
+package jav.comments;
+
+import resources.comments.CommentActions;
+import resources.comments.CommentValidator;
+
+/**
+ * Created by dima on 3.5.16.
+ */
+public class TestCommentActions {
+    private CommentActions commentActions;
+    private CommentValidator validator;
+
+}
